204b40748c08fc6163e108f6ae459fcc0310613e
Italo Souza <47384565+DIMAAGR@users.noreply.github.com>
2022-11-19 11:04:23 -0300

    task: geolocator - setting up

--- lib/src/controllers/global_controller.dart
+++ lib/src/controllers/global_controller.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 
 /// Para funcionar o GetX é necessário de uma classe de controle
@@ -13,7 +16,94 @@ class GlobalController extends GetxController {
   // Variables
   // ===========================================================================
 
-  final RxBool _isLoading = false.obs;
-  final RxDouble _lattitude = 0.0.obs;
-  final RxDouble _longetude = 0.0.obs;
+  final RxBool _isLoading = true.obs;
+  final RxDouble _latitude = 0.0.obs;
+  final RxDouble _longitude = 0.0.obs;
+
+  // ===========================================================================
+  // Create instances for them to be called
+  // ===========================================================================
+
+  RxBool checkIsLoading() => _isLoading;
+  RxDouble getLatitude() => _latitude;
+  RxDouble getLongitude() => _longitude;
+
+  // Dentro do Getx podemos adicionar ao controller funções de init e close.
+
+  // ===========================================================================
+  // onInit Method
+  // ===========================================================================
+
+  // it's similar to initState
+  @override
+  void onInit() {
+    //
+
+    if (_isLoading.isTrue) {
+      getLocation();
+    }
+
+    super.onInit();
+  }
+
+  // ===========================================================================
+  // onClose Method
+  // ===========================================================================
+
+  // it's similar to dispose
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  // ===========================================================================
+  // Methods
+  // ===========================================================================
+
+  getLocation() async {
+    //
+
+    bool isServiceEnabled;
+    LocationPermission locationPermission;
+
+    isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    // Return if is service is not enabled
+    if (!isServiceEnabled) {
+      return Future.error('Location is not enabled');
+    }
+
+    // check locationPermission status
+    locationPermission = await Geolocator.checkPermission();
+
+    // check if locationPermission is deniedForever
+    if (locationPermission == LocationPermission.deniedForever) {
+      return Future.error('Permission are denied forever');
+    }
+
+    // check if locationPermission is denied
+    else if (locationPermission == LocationPermission.denied) {
+      //
+
+      // request permission
+      locationPermission = await Geolocator.requestPermission();
+
+      // verify if locationPermission is not enabled
+      if (locationPermission == LocationPermission.denied) {
+        return Future.error('Location permission is denied');
+      }
+    }
+
+    // get the current user location
+    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high).then((location) {
+      //
+
+      // Update the latitude and longitude
+      _latitude.value = location.latitude;
+      _longitude.value = location.longitude;
+      _isLoading.value = false;
+    });
+
+    //
+  }
 }
